225a85a12f279987f84a198678cbeb5eb9cf9395
DaveWhiteh <david_whiteh@msn.com>
2023-06-06 10:40:41 +0000

    Fix errors that were stopping the requests to github api working

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -7,7 +7,7 @@ function userInformationHTML(user) {
         </h2>
         <div class="gh-content">
             <div class="gh-avatar">
-                <a href="${user.html_url} target="_blank">
+                <a href="${user.html_url}" target="_blank">
                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
@@ -21,12 +21,12 @@ function repoInformationHTML(repos) {
     }
 
     var listItemsHTML = repos.map(function(repo) {
-        return `<li>   
+        return `<li>
                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                 </li>`;
     });
 
-    return `<div class="clearfix repo-list>
+    return `<div class="clearfix repo-list">
                 <p>
                     <strong>Repo List:</strong>
                 </p>
@@ -37,8 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-    $("gh-user-data").html("");
-    $("gh-repo-data").html("");
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
 
     var username = $("#gh-username").val();
@@ -59,15 +59,18 @@ function fetchGitHubInformation(event) {
             function(firstResponse, secondResponse) {
                 var userData = firstResponse[0];
                 var repoData = secondResponse[0];
-                $("gh-user-data").html(userInformationHTML(userData));
-                $("gh-repo-data").html(repoInformationHTML(repoData));
+                $("#gh-user-data").html(userInformationHTML(userData));
+                $("#gh-repo-data").html(repoInformationHTML(repoData));
             }, function(errorResponse) {
                 if (errorResponse.status === 404) {
-                    $("gh-user-data").html(
+                    $("#gh-user-data").html(
                         `<h2>No info found for user ${username}</h2>`);
+                } else if (errorResponse.status === 403) {
+                    var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                    $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
                 } else {
                     console.log(errorResponse);
-                    $("gh-user-data").html(
+                    $("#gh-user-data").html(
                         `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
                 }
             });
